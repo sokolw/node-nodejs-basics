1bd79625b7afd83e4ae7f5cabc3ccc3bbb03c420
sokolw <sokolw21@gmail.com>
2022-06-03 22:06:53 +0300

    feat: read done

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,28 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+const targetFileName = 'fileToRead.txt';
+const workFolder = 'files';
+
 export const read = async () => {
-    // Write your code here 
+  try {
+    await isFileExist();
+    const readStream = (await fs.open(path.join('./', workFolder, targetFileName), 'r'))
+        .createReadStream();
+    readStream.pipe(process.stdout);
+  } catch (error) {
+    console.error(error);
+  }
 };
+
+const isFileExist = async () => {
+  const files = await fs.readdir(path.join('./', workFolder));
+
+  if (!files.includes(targetFileName)) {
+    throw new Error('FS operation failed');
+  }
+
+  return null;
+};
+
+read();
