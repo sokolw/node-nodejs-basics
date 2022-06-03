16f51ce175cc87fe9316490a663bf38d51437992
sokolw <sokolw21@gmail.com>
2022-06-03 22:07:04 +0300

    feat: write done

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,25 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+const targetFileName = 'fileToWrite.txt';
+const workFolder = 'files';
+
 export const write = async () => {
-    // Write your code here 
+  try {
+    const writeStream = (await fs.open(path.join('./', workFolder, targetFileName), 'w'))
+        .createWriteStream();
+
+    const readSteam = process.stdin;
+    readSteam.setEncoding('utf8');
+    readSteam.on('data', (chunk) => {
+      writeStream.write(chunk);
+      process.exit();
+    });
+
+    readSteam.read();
+  } catch (error) {
+    console.error(error);
+  }
 };
+
+write();
