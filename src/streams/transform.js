4b2de5ae14dd81ddaa6c4a1124f4a7afba431ebd
sokolw <sokolw21@gmail.com>
2022-06-03 22:51:21 +0300

    feat: done transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,16 @@
+import { pipeline } from 'stream/promises';
+import { EOL } from 'os';
+
 export const transform = async () => {
-    // Write your code here 
+  process.stdin.on('data', () => { process.stdin.emit('end'); });
+  await pipeline(process.stdin, reverseLine, process.stdout);
 };
+
+async function* reverseLine (source, { signal }) {
+  source.setEncoding('utf8');
+  for await (const chunk of source) {
+    yield await chunk.replace(new RegExp(EOL, 'gi'), '').split('').reverse().join('');
+  }
+}
+
+transform();
