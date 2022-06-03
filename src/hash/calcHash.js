a51150095f007e79c99c41825c7af2068a4aa0eb
sokolw <sokolw21@gmail.com>
2022-06-03 20:56:14 +0300

    feat: done calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,33 @@
+import fs from 'fs/promises';
+import path from 'path';
+import crypto from 'crypto';
+import { pipeline } from 'stream/promises';
+
+const targetFileName = 'fileToCalculateHashFor.txt';
+const workFolder = 'files';
+const typeHash = 'sha256';
+const hexadecimal = 'hex';
+
 export const calculateHash = async () => {
-    // Write your code here 
+  try {
+    await isFileExist();
+    const readStream = (await fs.open(path.join('./', workFolder, targetFileName), 'r'))
+        .createReadStream();
+    const hash = crypto.createHash(typeHash);
+    readStream.pipe(hash).setEncoding(hexadecimal).pipe(process.stdout);
+  } catch (error) {
+    console.error(error);
+  }
 };
+
+const isFileExist = async () => {
+  const files = await fs.readdir(path.join('./', workFolder));
+
+  if (!files.includes(targetFileName)) {
+    throw new Error('FS operation failed');
+  }
+
+  return null;
+};
+
+calculateHash();
