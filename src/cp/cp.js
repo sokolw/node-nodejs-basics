557e6d0f763e5a29fe656d2d71384a3a78eb3668
sokolw <sokolw21@gmail.com>
2022-06-04 23:47:15 +0300

    feat: done cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,21 @@
+import child from 'child_process';
+import path from 'path';
+
+const targetFileName = 'script.js';
+const workFolder = 'files';
+const command = 'node';
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+
+  const childProcess = child.fork(path.join(workFolder, targetFileName), [...args],
+    { stdio : ['pipe', 'pipe', 'pipe', 'ipc'] });
+  
+  process.stdin.pipe(childProcess.stdin);
+  childProcess.stdout.pipe(process.stdout);
+  
+  childProcess.on('exit', function (code) {
+    console.log('Child process exited with code ' + code);
+  });
 };
+
+spawnChildProcess(process.argv.slice(2));
