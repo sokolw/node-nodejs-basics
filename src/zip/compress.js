eb93213b0c24eeefaa8f9789c6e5d3228269fd9c
sokolw <sokolw21@gmail.com>
2022-06-04 09:16:36 +0300

    feat: done compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,23 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { pipeline } from 'stream/promises';
+import zlib from 'zlib';
+
+const targetFileName = 'fileToCompress.txt';
+const distFileName = 'archive.gz';
+const workFolder = 'files';
+
 export const compress = async () => {
-    // Write your code here 
+  try {
+    const gzip = zlib.createGzip();
+    const readStream = (await fs.open(path.join('./', workFolder, targetFileName), 'r'))
+      .createReadStream();
+    const writeStream = (await fs.open(path.join('./', workFolder, distFileName), 'w'))
+      .createWriteStream();
+    await pipeline(readStream, gzip, writeStream);
+  } catch (error) {
+    console.error(error);
+  }
 };
+
+compress();
