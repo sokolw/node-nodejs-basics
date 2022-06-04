432bebcfa70332eb69bb477a0f908e3c757edf04
sokolw <sokolw21@gmail.com>
2022-06-04 09:23:29 +0300

    feat: done decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,23 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { pipeline } from 'stream/promises';
+import zlib from 'zlib';
+
+const targetFileName = 'archive.gz';
+const distFileName = 'fileToCompress.txt';
+const workFolder = 'files';
+
 export const decompress = async () => {
-    // Write your code here 
+  try {
+    const gunzip = zlib.createGunzip();
+    const readStream = (await fs.open(path.join('./', workFolder, targetFileName), 'r'))
+      .createReadStream();
+    const writeStream = (await fs.open(path.join('./', workFolder, distFileName), 'w'))
+      .createWriteStream();
+    await pipeline(readStream, gunzip, writeStream);
+  } catch (error) {
+    console.error(error);
+  }
 };
+
+decompress();
