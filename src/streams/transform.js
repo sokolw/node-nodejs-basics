9a2516ca56acf38ba0dcbeaf4102239c1391e8e4
sokolw <sokolw21@gmail.com>
2022-06-05 09:16:12 +0300

    feat: remade transform implementation

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,16 +1,31 @@
 import { pipeline } from 'stream/promises';
 import { EOL } from 'os';
+import { Transform } from 'stream';
+
+class ReverseTransform extends Transform {
+  constructor(options) {
+    super(options);
+  }
+
+  _transform(data, encoding, callback) {
+    if (data.toString().toLowerCase() === `close${EOL}`) {
+      process.stdin.emit('end');
+      callback(null, '');
+    } else {
+      callback(null, data
+        .toString()
+        .replace(new RegExp(EOL, 'gi'), '')
+        .split('').reverse().join('') + EOL
+      );
+    }
+  };
+}
 
 export const transform = async () => {
-  process.stdin.on('data', () => { process.stdin.emit('end'); });
-  await pipeline(process.stdin, reverseLine, process.stdout);
+  const reverseTransform = new ReverseTransform();
+  process.stdin.on('end', () => { console.log('Bye, process close!'); });
+  console.log('Enter text to transform, for exit write : close');
+  await pipeline(process.stdin, reverseTransform, process.stdout);
 };
-
-async function* reverseLine (source, { signal }) {
-  source.setEncoding('utf8');
-  for await (const chunk of source) {
-    yield await chunk.replace(new RegExp(EOL, 'gi'), '').split('').reverse().join('');
-  }
-}
 
 transform();
