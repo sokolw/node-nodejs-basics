7e2f52595329800c285562507771511c59b9e001
sokolw <sokolw21@gmail.com>
2022-06-05 13:08:01 +0300

    feat: add tags for visualise transfer data

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,21 +1,58 @@
 import child from 'child_process';
 import path from 'path';
+import { Transform } from 'stream';
+import { EOL } from 'os';
+import { getPathFolder } from '../utilities.js'
 
 const targetFileName = 'script.js';
 const workFolder = 'files';
 const command = 'node';
 
-export const spawnChildProcess = async (args) => {
+class TagInput extends Transform {
+  constructor(options) {
+    super(options);
+  }
 
-  const childProcess = child.fork(path.join(workFolder, targetFileName), [...args],
-    { stdio : ['pipe', 'pipe', 'pipe', 'ipc'] });
+  _transform(data, encoding, callback) {
+    if (data.toString().toLowerCase() === `close${EOL}`) {
+      process.stdin.emit('end');
+      callback(null, '');
+    } else {
+      callback(null, `[data from main]->${data}`);
+    }
+  };
+}
+
+class TagOutput extends Transform {
+  constructor(options) {
+    super(options);
+  }
+
+  _transform(data, encoding, callback) {
+    if (data.toString().toLowerCase() === `close${EOL}`) {
+      process.stdin.emit('end');
+      callback(null, '');
+    } else {
+      callback(null, `[data from child]->${data}`);
+    }
+  };
+}
+
+export const spawnChildProcess = async (args) => {
+  const tagInput = new TagInput();
+  const tagOutput = new TagOutput();
+  const toScriptPath = path.join(getPathFolder(import.meta.url), workFolder, targetFileName);
   
-  process.stdin.pipe(childProcess.stdin);
-  childProcess.stdout.pipe(process.stdout);
+  const childProcess = child.fork(toScriptPath, [...args],
+    { stdio : ['pipe', 'pipe', 'pipe', 'ipc'] });
+
+  process.stdin.pipe(tagInput).pipe(childProcess.stdin);
+  childProcess.stdout.pipe(tagOutput).pipe(process.stdout);
   
   childProcess.on('exit', function (code) {
     console.log('Child process exited with code ' + code);
   });
 };
 
-spawnChildProcess(process.argv.slice(2));
+// call function for test
+await spawnChildProcess(process.argv.slice(2));
