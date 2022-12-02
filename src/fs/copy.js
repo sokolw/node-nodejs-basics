550984287570752c65f286f356880b7bf16cc9e2
sokolw <sokolw21@gmail.com>
2022-12-02 17:28:27 +0300

    feat: rework, add recursive copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,48 +1,41 @@
-import fs from 'fs/promises';
-import { pipeline } from 'stream/promises';
-import path from 'path';
-import { getPathFolder } from '../utilities.js';
+import fs from "fs/promises";
+import { pipeline } from "stream/promises";
+import path from "path";
+import { getPathFolder } from "../utilities.js";
 
-const targetFolder = 'files';
-const distFolder = 'files_copy';
+const targetFolder = "files";
+const distFolder = "files_copy";
 const scriptFolderPath = getPathFolder(import.meta.url);
 const targetPath = path.join(scriptFolderPath, targetFolder);
 const distPath = path.join(scriptFolderPath, distFolder);
 
-export const copy = async () => {
+export const copy = async (src = targetPath, dist = distPath) => {
   try {
-    await checkFolders();
-    await fs.mkdir(distPath);
-    const files = await fs.readdir(targetPath);
+    const files = await fs.readdir(src, { withFileTypes: true });
+    await fs.mkdir(dist, { recursive: false });
 
     for (const file of files) {
-      const readStream = (await fs.open(path.join(targetPath, file), 'r'))
-          .createReadStream();
-      const writeStream = (await fs.open(path.join(distPath, file), 'w'))
-          .createWriteStream();
-      await pipeline(readStream, writeStream);
+      if (file.isDirectory()) {
+        await copy(path.join(src, file.name), path.join(dist, file.name));
+      } else {
+        const readStream = (
+          await fs.open(path.join(src, file.name), "r")
+        ).createReadStream();
+        const writeStream = (
+          await fs.open(path.join(dist, file.name), "w")
+        ).createWriteStream();
+        await pipeline(readStream, writeStream);
+      }
     }
-  } catch (error) {
-    console.error(error);
+  } catch {
+    throw new Error("FS operation failed");
   }
-};
-
-const checkFolders = async () => {
-  const files = await fs.readdir(path.join(scriptFolderPath), { withFileTypes : true });
-  let tempDirs = [];
-
-  for (const file of files) {
-    if (file.isDirectory()) {
-      tempDirs.push(file.name);
-    }
-  }
-
-  if (!tempDirs.includes(targetFolder) || tempDirs.includes(distFolder)) {
-    throw new Error('FS operation failed');
-  }
-
-  return null;
 };
 
 // call function for test
-await copy();
+try {
+  await copy();
+  console.log("Done.");
+} catch (error) {
+  console.error(error);
+}
