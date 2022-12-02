ac6c9c31143b9561dad5cf1ff0ad6b64a2faf6eb
sokolw <sokolw21@gmail.com>
2022-12-02 16:53:05 +0300

    feat: rework solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -9,24 +9,16 @@ const taskContent = 'I am fresh and young';
 
 export const create = async () => {
   try {
-    await isFileExist();
-    const file = await fs.open(fullPath, 'w');
-    const writeStream = file.createWriteStream();
-    writeStream.write(taskContent);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-const isFileExist = async () => {
-  try {
-    const file = await fs.open(fullPath, 'r');
+    await fs.writeFile(fullPath, taskContent, { flag: 'wx+' });
   } catch {
-    return null;
+    throw new Error('FS operation failed');
   }
-
-  throw new Error('FS operation failed');
 };
 
 // call function for test
-await create();
+try {
+  await create();
+  console.log('Done.');
+} catch (error) {
+  console.error(error);
+}
